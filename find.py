29c05ae6e3a195917a493dbf4d3bd6b06cb48a82
Martin Olveyra <molveyra@gmail.com>
2017-10-24 00:11:18 -0300

    updated find method to allow to set hamming distance

--- find.py
+++ find.py
@@ -3,12 +3,14 @@ Finds the position of a pattern in a text
 """
 import argparse
 
-def find(text, pattern):
+import hamming
+
+def find(text, pattern, hamming_distance=0):
     pos = []
     n = len(text)
     k = len(pattern)
     for i in range(0, n - k + 1):
-        if text[i:i+k] == pattern:
+        if hamming.hamming(text[i:i+k], pattern) <= hamming_distance:
             pos.append(i)
     return pos
 
@@ -17,6 +19,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("text", metavar="TEXTFILE", type=argparse.FileType('r'))
     parser.add_argument("pattern")
+    parser.add_argument("--hamming", type=int, default=0)
 
     args = parser.parse_args()
-    print ' '.join(`i` for i in find(args.text.read().strip(), args.pattern))
+    print ' '.join(`i` for i in find(args.text.read().strip(), args.pattern, args.hamming))
