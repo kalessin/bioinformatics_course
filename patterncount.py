480db6258455fd2363bd4a7a5e1bf3d80a67e52c
Martin Olveyra <molveyra@gmail.com>
2017-10-25 13:34:06 -0300

    pattern count with hamming distance

--- patterncount.py
+++ patterncount.py
@@ -2,13 +2,14 @@
 Counts the ocurrences of a pattern in a text
 """
 import argparse
+import hamming
 
-def patterncount(text, pattern):
+def patterncount(text, pattern, d):
     count = 0
     k = len(pattern)
     n = len(text)
     for i in range(0, n - k + 1):
-        if text[i:i+k] == pattern:
+        if hamming.hamming(text[i:i+k], pattern) <= d:
             count += 1
     return count
 
@@ -17,6 +18,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("text", metavar="TEXTFILE", type=argparse.FileType('r'))
     parser.add_argument("pattern")
+    parser.add_argument("--hamming", type=int, default=0)
 
     args = parser.parse_args()
-    print "count: %d" % patterncount(args.text.read(), args.pattern)
+    print "count: %d" % patterncount(args.text.read(), args.pattern, args.hamming)
