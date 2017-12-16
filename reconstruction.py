af806629d0a2b8cdd567694e5ec3c80778a084fb
Martin Olveyra <molveyra@gmail.com>
2017-12-16 17:45:36 -0300

    added overlap graph

--- reconstruction.py
+++ reconstruction.py
@@ -1,6 +1,7 @@
-def composition(k, text):
+def decomposition(k, text):
     """
-    >>> composition(5, 'CAATCCAAC')
+    decompose a string into a sequence of overlapping k-mers
+    >>> decomposition(5, 'CAATCCAAC')
     ['AATCC', 'ATCCA', 'CAATC', 'CCAAC', 'TCCAA']
     """
     n = len(text)
@@ -10,9 +11,10 @@ def composition(k, text):
     return sorted(result)
 
 
-def reconstruct_from_sorted_kmers(kmers):
+def compose_from_sorted_kmers(kmers):
     """
-    >>> reconstruct_from_sorted_kmers(['ACCGA', 'CCGAA', 'CGAAG', 'GAAGC', 'AAGCT'])
+    compose original string from overlapping k-mers decomposition sequence
+    >>> compose_from_sorted_kmers(['ACCGA', 'CCGAA', 'CGAAG', 'GAAGC', 'AAGCT'])
     'ACCGAAGCT'
     """
     result, kmers = kmers[0], kmers[1:]
@@ -20,4 +22,27 @@ def reconstruct_from_sorted_kmers(kmers):
         k = len(kmer)
         assert kmer[0:k-1] == result[-k+1:]
         result += kmer[-1]
+    return result
+
+
+def overlap_graph(kmers):
+    """
+    For each k-mer in a sequence, generate a list of overlapping k-mers from it
+    >>> overlap_graph(['ATGCG', 'GCATG', 'CATGC', 'AGGCA', 'GGCAT'])
+    [('GCATG', ['CATGC']), ('CATGC', ['ATGCG']), ('AGGCA', ['GGCAT']), ('GGCAT', ['GCATG'])]
+    >>> overlap_graph(['AAAAA', 'AAAAA', 'AAAAC', 'AAACG', 'AACGA', 'ACGAT'])
+    [('AAAAA', ['AAAAA', 'AAAAC']), ('AAAAA', ['AAAAA', 'AAAAC']), ('AAAAC', ['AAACG']), ('AAACG', ['AACGA']), ('AACGA', ['ACGAT'])]
+    """
+    result = []
+    for i in range(len(kmers)):
+        kmer = kmers[i]
+        k = len(kmer)
+        ovp = []
+        for j in range(len(kmers)):
+            if i != j:
+                kkmer = kmers[j]
+                if kkmer[0:k-1] == kmer[-k+1:]:
+                    ovp.append(kkmer)
+        if ovp:
+            result.append((kmer, ovp))
     return result
